0d7999d02eddedd77c3c11ace8dbea12f139b69e
Grzegorz Szpetkowski <grzegorz.szpetkowski@student.uj.edu.pl>
2015-11-09 23:12:38 +0100

    Implement basic Game class, that is responsible for displaying and handling close event for an empty window by the SDL 2.0 API. The main function contains game loop, that is repeatedly checking for running condition.

---
+++ Shooter/Game.h
@@ -0,0 +1,29 @@
+#include "SDL.h"
+
+#ifndef __Game__
+#define __Game__
+
+class Game
+{
+public:
+    Game() {}
+    ~Game() {}
+
+    bool init(const char* title, int xpos, int ypos,
+        int width, int height, int flags);
+    void render();
+    void update();
+    void handleEvents();
+    void clean();
+
+    // a function to access the private running variable
+    bool running() { return m_bRunning; }
+
+private:
+    SDL_Window *m_pWindow;
+    SDL_Renderer *m_pRenderer;
+
+    bool m_bRunning;
+};
+
+#endif /* defined(__Game__) */
